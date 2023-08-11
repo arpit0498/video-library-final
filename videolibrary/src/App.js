a2cb20ab0ade8f863449d1a2c77f026fd1e5211c
arpit gupta <mycontact.arpit@gmail.com>
2023-08-11 21:36:44 +0530

    update

---
+++ videolibrary/src/App.js
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import { LeftSidebar } from "./component/LeftSidebar/LeftSidebar";
+import { Homepage } from "./pages/Homepage/Homepage";
+import { VideoListing } from "./pages/VideoListing/VideoListing";
+import { WatchLater } from "./pages/WatchLater/WatchLater";
+import { VideoDetail } from "./pages/VideoDetail/VideoDetail";
+import { Explore } from "./pages/Explore/Explore";
+import { Playlists } from "./pages/Playlists/Playlists";
+import { PlaylistMusicVideos } from "./pages/PlaylistMusicVideos/PlaylistMusicVideos";
+// import { WatchLater } from "./pages/WatchLater/WatchLater";
+
+
+function App() {
+  return (
+    <div className="main-container">
+      <LeftSidebar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/:videoCategory" element={<VideoListing />} />
+        <Route path="/video/:videoTitle" element={<VideoDetail />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/playlist" element={<Playlists />} />
+        <Route path="/playlist/:playlistId" element={<PlaylistMusicVideos />} />
+        <Route path="/watch-later" element={<WatchLater />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
